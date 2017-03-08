04befe209bc71b48c6c58e13b17354d77b01ebbf
Matt <ionize@163.com>
2017-03-08 21:33:45 +0800

    Commit two: add function--Calculate prime number

--- myExp/myExp/myExp.py
+++ myExp/myExp/myExp.py
@@ -1,3 +1,15 @@
 import os
+import math
+import requests
 
+#First statement
 print "Hello! Guys:)"
+
+for parent in range(3,1000000):
+    iNum=0
+    for child in range(2,int(math.sqrt(parent))):
+        if (parent%child)==0:
+            iNum+=1
+    if iNum==0:
+        print parent
+                
